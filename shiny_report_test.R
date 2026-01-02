b95a5f1470b4198f36355fa1de3ff7fd27c0fc29
Raby <0492634791@FEMA.DHS.GOV>
2026-01-02 15:23:10 -0500

    add logo, font etc

--- shiny_report_test.R
+++ shiny_report_test.R
@@ -15,6 +15,17 @@ library(DT)
 # counties first
 layer_url <- "https://services.arcgis.com/XG15cJAlne2vxtgt/arcgis/rest/services/National_Risk_Index_Counties/FeatureServer/0"
 
+# get field display names
+get_field_aliases <- function(layer_url) {
+  res <- POST(paste0(layer_url, "?f=json"))
+  txt <- content(res, "text")
+  parsed <- fromJSON(txt)
+  # parsed$fields is a data frame: columns "name" and "alias"
+  aliases <- setNames(parsed$fields$alias, parsed$fields$name)
+  return(aliases)
+}
+field_aliases <- get_field_aliases(layer_url)
+
 # query REST API to populate county drop down menu
 get_county_list <- function(layer_url, n = 100) {
   res <- POST(paste0(layer_url, "/query"), body = list(
@@ -33,7 +44,7 @@ get_county_list <- function(layer_url, n = 100) {
 }
 
 county_list <- get_county_list(layer_url, 100)
-print(county_list)
+# print(county_list)
 
 # app --------------------------------------------------------------------------
 ui <- fluidPage(
@@ -51,7 +62,7 @@ ui <- fluidPage(
 )
 
 server <- function(input, output, session) {
-  # Function to get full attributes for a selected county by OBJECTID
+  # get full attributes for selected county by OBJECTID
   get_county_attributes <- function(objectid) {
     res <- POST(paste0(layer_url, "/query"), body = list(
       where = paste0("OBJECTID=", objectid),
@@ -69,7 +80,7 @@ server <- function(input, output, session) {
     }
   }
   
-  # Render the selected county's attributes as a table
+  # selected county's attributes as a table
   output$county_table <- DT::renderDT({
     req(input$county)
     attrs <- get_county_attributes(input$county)
@@ -83,11 +94,26 @@ server <- function(input, output, session) {
     },
     content = function(file) {
       attrs <- get_county_attributes(input$county)
+      # switch to display names
+      field_names <- colnames(attrs)
+      display_names <- ifelse(
+        field_names %in% names(field_aliases),
+        field_aliases[field_names],
+        field_names
+      )
+      vertical_table <- data.frame(
+        Attribute = display_names,
+        Value = as.character(as.vector(t(attrs))),
+        stringsAsFactors = FALSE
+      )
+      # take out unneeded columns
+      remove_aliases <- c("OBJECTID", "Shape__Area", "Shape__Length")
+      vertical_table_filtered <- vertical_table[!(vertical_table$Attribute %in% remove_aliases), ]
       params <- list(
-        county_attributes = attrs
+        county_attributes = vertical_table_filtered
       )
       rmarkdown::render(
-        input = here("spatial_data_scientist", "RAPT", "NRI", "report", "report_template.Rmd"),
+        input = here("spatial_data_scientist", "RAPT", "NRI", "report", "nri_report", "report_template.Rmd"),
         output_file = file,
         params = params,
         envir = new.env(parent = globalenv())
@@ -99,7 +125,6 @@ server <- function(input, output, session) {
 
 shinyApp(ui, server)
 
-# first got proof of concept
 # NEXT
 # format table
 # then add state filter 
